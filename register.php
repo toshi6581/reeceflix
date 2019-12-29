dcbe03892499929195fa045d80371983fa70c3d0
AraiEntap <arai@entap.co.jp>
2019-12-29 15:21:42 +0900

    Done 8. Creating the input fields

---
+++ register.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Welcome to Reeceflix</title>
+    <link rel="stylesheet" type="text/css" href="assets/style/style.css">
+</head>
+<body>
+<div class="signInContainer">
+
+    <div class="column">
+        <form action="" method="POST">
+            <input type="text" name="firstName" placeholder="First name" required>
+            <input type="text" name="lastName" placeholder="Last name" required>
+            <input type="text" name="username" placeholder="Username" required>
+            <input type="email" name="email" placeholder="Email" required>
+            <input type="email" name="email2" placeholder="Confirm email" required>
+            <input type="password" name="password" placeholder="Password" required>
+            <input type="password" name="password2" placeholder="Confirm password" required>
+            <input type="submit" name="submitPassword" value="SUBMIT">
+        </form>
+    </div>
+</div>
+</body>
+</html>
