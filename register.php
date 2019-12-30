7153557625d03b5a13624fd2fc134682d1e08044
AraiEntap <arai@entap.co.jp>
2019-12-30 12:43:45 +0900

    Done 15. Sanitising the user's first name

--- register.php
+++ register.php
@@ -1,6 +1,16 @@
 <?php
   if(isset($_POST["submitButton"])){
-    echo "From was submitted";
+
+    $firstName = sanitizeFormString($_POST["firstName"]);
+
+  }
+
+  function sanitizeFormString($inputText){
+    $inputText = strip_tags($inputText);
+    $inputText = srt_replace(" ", "", $inputText);
+    $inputText = strtolower($inputText);
+    $inputText = ucfirst($inputText);
+    return $inputText;
   }
 ?>
 
